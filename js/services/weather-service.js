4ac06a85b52e996f3affab427b7cbfa30ea03a41
Eyal Barkai <eyalskl18@gmail.com>
2020-06-18 00:22:49 +0300

    one more

--- js/services/weather-service.js
+++ js/services/weather-service.js
@@ -6,7 +6,7 @@ export const weatherService = {
 
 
 function getWeather({latitude, longitude}) {
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=8519f17bd88c6fddc42c03e6ce32f705`)
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=84212711cf97e358b82730a3c4f6a10b`)
         .then(weather => weather.json());
 }
 
