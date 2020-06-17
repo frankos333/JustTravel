d7815c03f240419dc877cf456cc4a3739523935d
Eyal Barkai <eyalskl18@gmail.com>
2020-06-17 23:19:57 +0300

    abc

--- js/services/geo-service.js
+++ js/services/geo-service.js
@@ -1,10 +1,16 @@
 'use strict'
 export const geoService = {
-    getGeo
+    getGeo,
+    getGeoByLocation
 }
 
 function getGeo(address) {
     var addPrm = fetch(`https://maps.googleapis.com/maps/api/geocode/json?&address=${address}&key=AIzaSyB6VnvgD2vcWlBcPoKJUvF1bAdMbdQo25M`)
         .then(coords => coords.json())
     return Promise.resolve(addPrm)
+}
+
+function getGeoByLocation(lat, lng) {
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyB6VnvgD2vcWlBcPoKJUvF1bAdMbdQo25M`)
+        .then(coords => coords.json())
 }
